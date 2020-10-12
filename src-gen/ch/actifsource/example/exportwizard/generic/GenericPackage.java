c9af12954b3a7e6f05083b6fbc32bff2bf3c5172
ruti <grischa.rutishauser@actifsource.com>
2020-10-12 11:54:11 +0200

    Add model to export

---
+++ src-gen/ch/actifsource/example/exportwizard/generic/GenericPackage.java
@@ -0,0 +1,26 @@
+package ch.actifsource.example.exportwizard.generic;
+
+import ch.actifsource.core.Resource;
+
+public class GenericPackage {
+  
+  /* Begin Protected Region [[user]] */
+  
+  /* End Protected Region   [[user]] */
+  
+  private GenericPackage() {}
+  
+  public static final String getScopeName() {
+    return "ch.actifsource.example.exportwizard";
+  }
+  
+  public static final String getResourceFolderPath() {
+    return "asrc";
+  }
+  
+  public static final String getPackageName() {
+    return "ch.actifsource.example.exportwizard.generic";
+  }
+  
+}
+/* Actifsource ID=[c1a07b92-fc09-11e0-b756-0dc1d96a76cb,ch.actifsource.example.exportwizard.generic,RcPHIXRRR4IybRO3jvhfOmRdel4=] */
